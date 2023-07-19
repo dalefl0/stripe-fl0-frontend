64b7745897a4dbead962b56b468ef98e31270b80
Dale Brett <dalefl0@outlook.com>
2023-07-19 06:52:42 +0530

    add: add backend url

--- src/components/PricingPlans.jsx
+++ src/components/PricingPlans.jsx
@@ -3,86 +3,86 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const PricingPlans = ({ tiers, yearly, setTier, tier }) => {
-  const email = localStorage.getItem("email");
+	const email = localStorage.getItem("email");
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const handleCheckout = (product) => {
-    axios
-      .post(
-        `${
-          import.meta.env.VITE_APP_API_BASE_URL
-        }/api/v1/stripe/create-checkout-session`,
-        {
-          email,
-          product,
-        },
-      )
-      .then((res) => {
-        if (res.data.url) {
-          setTier(product.name);
-          localStorage.setItem("tier", product.name);
-          window.location.href = res.data.url;
-        }
-      })
-      .catch((err) => navigate("/cancel"));
-  };
+	const handleCheckout = (product) => {
+		axios
+			.post(
+				`https://stripe-fl0-backend-dev.fl0.io/api/v1/stripe/create-checkout-session`,
+				{
+					email,
+					product,
+				}
+			)
+			.then((res) => {
+				if (res.data.url) {
+					setTier(product.name);
+					localStorage.setItem("tier", product.name);
+					window.location.href = res.data.url;
+				}
+			})
+			.catch((err) => navigate("/cancel"));
+	};
 
-  return (
-    <>
-      <div className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4">
-        {tiers.map((item) => (
-          <div
-            key={item.name}
-            className="border border-gray-200 rounded-lg shadow-sm divide-y divide-gray-200">
-            <div className="p-6">
-              <h2 className="text-lg leading-6 font-medium text-gray-900">
-                {item.name}
-              </h2>
-              <p className="mt-4 text-sm text-gray-500">{item.description}</p>
-              <p className="mt-8">
-                <span className="text-4xl font-extrabold text-gray-900">
-                  ${item.priceMonthly}
-                </span>{" "}
-                <span className="text-base font-medium text-gray-500">
-                  ${yearly ? "/yr" : "/mo"}
-                </span>
-              </p>
-              <button
-                onClick={() =>
-                  handleCheckout({
-                    name: item.name,
-                    price: item.priceMonthly,
-                    productOwner: "Test",
-                    description: item.description,
-                    quantity: 1,
-                  })
-                }
-                className="mt-8 block w-full bg-gray-800 border border-gray-800 rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-gray-900">
-                Buy {item.name}
-              </button>
-            </div>
-            <div className="pt-6 pb-8 px-6">
-              <h3 className="text-xs font-medium text-gray-900 tracking-wide uppercase">
-                What's included
-              </h3>
-              <ul role="list" className="mt-6 space-y-4">
-                {item.includedFeatures.map((feature) => (
-                  <li key={feature} className="flex space-x-3">
-                    <CheckIcon
-                      className="flex-shrink-0 h-5 w-5 text-green-500"
-                      aria-hidden="true"
-                    />
-                    <span className="text-sm text-gray-500">{feature}</span>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4">
+				{tiers.map((item) => (
+					<div
+						key={item.name}
+						className="border border-gray-200 rounded-lg shadow-sm divide-y divide-gray-200"
+					>
+						<div className="p-6">
+							<h2 className="text-lg leading-6 font-medium text-gray-900">
+								{item.name}
+							</h2>
+							<p className="mt-4 text-sm text-gray-500">{item.description}</p>
+							<p className="mt-8">
+								<span className="text-4xl font-extrabold text-gray-900">
+									${item.priceMonthly}
+								</span>{" "}
+								<span className="text-base font-medium text-gray-500">
+									${yearly ? "/yr" : "/mo"}
+								</span>
+							</p>
+							<button
+								onClick={() =>
+									handleCheckout({
+										name: item.name,
+										price: item.priceMonthly,
+										productOwner: "Test",
+										description: item.description,
+										quantity: 1,
+									})
+								}
+								className="mt-8 block w-full bg-gray-800 border border-gray-800 rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-gray-900"
+							>
+								Buy {item.name}
+							</button>
+						</div>
+						<div className="pt-6 pb-8 px-6">
+							<h3 className="text-xs font-medium text-gray-900 tracking-wide uppercase">
+								What's included
+							</h3>
+							<ul role="list" className="mt-6 space-y-4">
+								{item.includedFeatures.map((feature) => (
+									<li key={feature} className="flex space-x-3">
+										<CheckIcon
+											className="flex-shrink-0 h-5 w-5 text-green-500"
+											aria-hidden="true"
+										/>
+										<span className="text-sm text-gray-500">{feature}</span>
+									</li>
+								))}
+							</ul>
+						</div>
+					</div>
+				))}
+			</div>
+		</>
+	);
 };
 
 export default PricingPlans;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,115 +3,119 @@ import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const Signup = ({ email, setEmail, password, setPassword }) => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const localStorageEmail = localStorage.getItem("email");
+	const localStorageEmail = localStorage.getItem("email");
 
-  useEffect(() => {
-    if (localStorageEmail) {
-      navigate("/");
-    }
-  }, []);
+	useEffect(() => {
+		if (localStorageEmail) {
+			navigate("/");
+		}
+	}, []);
 
-  const onSubmit = (e) => {
-    e.preventDefault();
+	const onSubmit = (e) => {
+		e.preventDefault();
 
-    axios
-      .post(`${import.meta.env.VITE_APP_API_BASE_URL}/api/v1/users/signup`, {
-        email: email,
-        password: password,
-      })
-      .then(() => {
-        localStorage.setItem("email", email);
-        navigate("/");
-      })
-      .catch((err) => navigate("/signup"));
-  };
+		axios
+			.post(`https://stripe-fl0-backend-dev.fl0.io/api/v1/users/signup`, {
+				email: email,
+				password: password,
+			})
+			.then(() => {
+				localStorage.setItem("email", email);
+				navigate("/");
+			})
+			.catch((err) => navigate("/signup"));
+	};
 
-  return (
-    <>
-      <div className="h-screen flex">
-        <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-          <div className="mx-auto w-full max-w-sm lg:w-96">
-            <div>
-              <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
-                Sign up to your account
-              </h2>
-              <p className="mt-2 text-sm text-gray-600">
-                Or{" "}
-                <Link
-                  to="/login"
-                  className="font-medium text-indigo-600 hover:text-indigo-500">
-                  Log In
-                </Link>
-              </p>
-            </div>
+	return (
+		<>
+			<div className="h-screen flex">
+				<div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+					<div className="mx-auto w-full max-w-sm lg:w-96">
+						<div>
+							<h2 className="mt-6 text-3xl font-extrabold text-gray-900">
+								Sign up to your account
+							</h2>
+							<p className="mt-2 text-sm text-gray-600">
+								Or{" "}
+								<Link
+									to="/login"
+									className="font-medium text-indigo-600 hover:text-indigo-500"
+								>
+									Log In
+								</Link>
+							</p>
+						</div>
 
-            <div className="mt-8">
-              <div className="mt-6">
-                <form onSubmit={onSubmit} className="space-y-6">
-                  <div>
-                    <label
-                      htmlFor="email"
-                      className="block text-sm font-medium text-gray-700">
-                      Email address
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        id="email"
-                        value={email}
-                        name="email"
-                        type="email"
-                        autoComplete="email"
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                      />
-                    </div>
-                  </div>
+						<div className="mt-8">
+							<div className="mt-6">
+								<form onSubmit={onSubmit} className="space-y-6">
+									<div>
+										<label
+											htmlFor="email"
+											className="block text-sm font-medium text-gray-700"
+										>
+											Email address
+										</label>
+										<div className="mt-1">
+											<input
+												id="email"
+												value={email}
+												name="email"
+												type="email"
+												autoComplete="email"
+												onChange={(e) => setEmail(e.target.value)}
+												required
+												className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+											/>
+										</div>
+									</div>
 
-                  <div className="space-y-1">
-                    <label
-                      htmlFor="password"
-                      className="block text-sm font-medium text-gray-700">
-                      Password
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        id="password"
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        type="password"
-                        autoComplete="current-password"
-                        required
-                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                      />
-                    </div>
-                  </div>
+									<div className="space-y-1">
+										<label
+											htmlFor="password"
+											className="block text-sm font-medium text-gray-700"
+										>
+											Password
+										</label>
+										<div className="mt-1">
+											<input
+												id="password"
+												name="password"
+												value={password}
+												onChange={(e) => setPassword(e.target.value)}
+												type="password"
+												autoComplete="current-password"
+												required
+												className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+											/>
+										</div>
+									</div>
 
-                  <div>
-                    <button
-                      type="submit"
-                      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
-                      Sign up
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="hidden lg:block relative w-0 flex-1">
-          <img
-            className="absolute inset-0 h-full w-full object-cover"
-            src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
-            alt=""
-          />
-        </div>
-      </div>
-    </>
-  );
+									<div>
+										<button
+											type="submit"
+											className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+										>
+											Sign up
+										</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="hidden lg:block relative w-0 flex-1">
+					<img
+						className="absolute inset-0 h-full w-full object-cover"
+						src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+						alt=""
+					/>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Signup;
